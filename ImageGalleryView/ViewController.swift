abe1336f56b968f10ab744f13970861e8bf49926
upmer <upmer@upmer.com>
2019-12-23 00:50:58 +0800

    feat: setup view

--- ImageGalleryView/ViewController.swift
+++ ImageGalleryView/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  weak var scrollView: UIScrollView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    let scrollView = UIScrollView()
+    self.scrollView = scrollView
+    view.addSubview(scrollView)
+    scrollView.isPagingEnabled = true
+    scrollView.frame = view.bounds
+    
+    scrollView.contentSize = CGSize(width: 3 * view.bounds.width, height: 0)
+    
+    for i in 0...2 {
+      let s = UIScrollView()
+      s.delegate = self
+      s.frame = CGRect(x: view.bounds.width * CGFloat(i), y: 0, width: view.bounds.width, height: view.bounds.height)
+      scrollView.addSubview(s)
+      s.maximumZoomScale = 3
+      s.minimumZoomScale = 0.5
+      let imageView = UIImageView()
+      imageView.frame = view.bounds
+      imageView.image = UIImage(named: "1.jpeg")
+      s.addSubview(imageView)
+    }
   }
 
 
+}
+
+extension ViewController: UIScrollViewDelegate {
+  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+    return scrollView.subviews.first
+  }
 }
 
